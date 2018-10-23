3eea528132d8e20005d4453882cfd41682b806b1
Wayne Collier <ke4tri@gmail.com>
2018-10-22 21:33:28 -0500

    working

--- javascripts/data/boardsData.js
+++ javascripts/data/boardsData.js
@@ -2,7 +2,6 @@ const loadBoards = () => {
     return new Promise((resolve, reject) => {
         $.get('../db/boards.json')
             .done((data) => {
-                console.log( data.boards)
                 resolve(data.boards);
             })
             .fail((error) => {
